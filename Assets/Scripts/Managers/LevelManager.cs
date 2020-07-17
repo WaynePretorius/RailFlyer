664303519aaaf1adb0f102aa877fca7b2c364e92
WaynePretorius <pretorius.wayne83@hotmail.com>
2020-07-17 12:30:19 +0200

    Added extra rail features to start off. added splash screen. Added music and level manager as wsell

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    [SerializeField] private float timeToLoadNextScene = 5f;
+
+    private void Awake()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (sceneIndex <= 0)
+        {
+            StartCoroutine(LoadFromSplash());
+        }
+    }
+
+    private IEnumerator LoadFromSplash()
+    {
+        yield return new WaitForSeconds(timeToLoadNextScene);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}
---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        int managers = FindObjectsOfType<SoundManager>().Length;
+
+        if(managers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
